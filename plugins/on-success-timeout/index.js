a4e1110fff5cedbec1f227c55ba39622f3c0f518
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2023-03-24 13:53:46 +0000

    common js

--- plugins/on-success-timeout/index.js
+++ plugins/on-success-timeout/index.js
@@ -1,10 +1,12 @@
-export const onSuccess = async function () {
-  console.log("------ STARTING PLACEHOLDER ONSUCCESS PLUGIN --------");
-  //   2 mins
-  await timeout(120000);
-  console.log("------ FINISH PLACEHOLDER ONSUCCESS PLUGIN --------");
-};
+module.exports = {
+  onSuccess: async function () {
+    function timeout(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+    console.log("------ STARTING PLACEHOLDER ONSUCCESS PLUGIN --------");
+    //   2 mins
+    await timeout(120000);
+    console.log("------ FINISH PLACEHOLDER ONSUCCESS PLUGIN --------");
+  },
+};
